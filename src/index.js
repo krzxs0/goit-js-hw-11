3b509e37d59c0e65864f43bba0b38defe8627ec2
krzxs0 <krzxssamp@gmail.com>
2023-06-26 13:32:15 +0300

    12312312

--- src/index.js
+++ src/index.js
@@ -1,83 +1,104 @@
-import {getImages} from "./api.js";
+import { getImages } from "./api.js";
 
-let inputElement = document.getElementById('inputSearch')
-let button = document.getElementById('submitButton')
-let loadmore = document.getElementsByClassName('load-more')[0]
+let inputElement = document.getElementById('inputSearch');
+let button = document.getElementById('submitButton');
+let loadmore = document.getElementsByClassName('load-more')[0];
 let galleryList = document.querySelector('.gallery');
-let pageCounter = 1
+let pageCounter = 1;
+let photos = [];
 
 loadmore.onclick = async () => {
-    let query = await getImages(inputElement.value, pageCounter)
-
-    pageCounter++
-
-    query.data.hits.forEach(hit => {
-        photos.push({
-            imgURL: hit.webformatURL,
-            tags: hit.tags,
-            likes: hit.likes,
-            views: hit.views,
-            comments: hit.comments,
-            downloads: hit.downloads
-        })
-    });
-
-    galleryList.innerHTML = photos.map(createGalleryItem).join('')
-}
-
-let photos = []
+  await loadMoreImages();
+};
 
 button.onclick = async () => {
-    if(inputElement.value.length == 0) return Notiflix.Notify.failure('Please enter something and try again!');
+  await searchImages();
+};
 
-    let query = await getImages(inputElement.value)
-
-    if(query.data.totalHits == 0) return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    else Notiflix.Notify.success(`Hooray! we found ${query.data.totalHits}`)
-
-    loadmore.style.display = 'block'
-
-    query.data.hits.forEach(hit => {
-        photos.push({
-            imgURL: hit.webformatURL,
-            tags: hit.tags,
-            likes: hit.likes,
-            views: hit.views,
-            comments: hit.comments,
-            downloads: hit.downloads
-        })
-    });
-
-    galleryList.innerHTML = photos.map(createGalleryItem).join('')
-
-    pageCounter++
-    
-    photos = []
-}
-
-
-const createGalleryItem = ({ imgURL, tags, likes, views, comments, downloads }) => `
-<div class="photo-card">
-        <img src="${imgURL}" alt="${tags}" loading="lazy" />
-        
-        <div class="info">
-          <p class="info-item">
-            <b>Likes ${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads ${downloads}</b>
-          </p>
-        </div>
- </div>
-`;
 const handleKeyPress = async (event) => {
   if (event.key === 'Enter') {
+    event.preventDefault(); 
     await searchImages();
   }
 };
+
+inputElement.addEventListener('keypress', handleKeyPress);
+
+const searchImages = async () => {
+  if (inputElement.value.length === 0) {
+    Notiflix.Notify.failure('Please enter something and try again!');
+    return;
+  }
+
+  let query = await getImages(inputElement.value);
+
+  if (query.data.totalHits === 0) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
+  } else {
+    Notiflix.Notify.success(`Hooray! we found ${query.data.totalHits}`);
+  }
+
+  loadmore.style.display = 'block';
+
+  query.data.hits.forEach((hit) => {
+    photos.push({
+      imgURL: hit.webformatURL,
+      tags: hit.tags,
+      likes: hit.likes,
+      views: hit.views,
+      comments: hit.comments,
+      downloads: hit.downloads,
+    });
+  });
+
+  galleryList.innerHTML = photos.map(createGalleryItem).join('');
+
+  pageCounter++;
+
+  photos = [];
+};
+
+const loadMoreImages = async () => {
+  let query = await getImages(inputElement.value, pageCounter);
+
+  pageCounter++;
+
+  query.data.hits.forEach((hit) => {
+    photos.push({
+      imgURL: hit.webformatURL,
+      tags: hit.tags,
+      likes: hit.likes,
+      views: hit.views,
+      comments: hit.comments,
+      downloads: hit.downloads,
+    });
+  });
+
+  galleryList.innerHTML = photos.map(createGalleryItem).join('');
+};
+
+const createGalleryItem = ({ imgURL, tags, likes, views, comments, downloads }) => `
+  <div class="photo-card">
+    <img src="${imgURL}" alt="${tags}" loading="lazy" />
+    
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${downloads}</b>
+      </p>
+    </div>
+  </div>
+`;
+
+
+
+
